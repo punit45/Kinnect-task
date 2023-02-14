1607a23a0fea76215f08e45b7fc5998ad68f731a
punit45 <ipunit96@gmail.com>
2023-02-15 03:58:44 +0530

    task completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'addgame';
+  num1: any = Math.floor(Math.random() * 10);
+  num2: any = Math.floor(Math.random() * 10);
+  answer: any = '';
+  message: any;
+  totalAns: number = 0;
+  totaltime: number = 0;
+  avg: any;
+  e: any;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.totaltime += 1;
+    }, 1000);
+  }
+
+  randomGen() {
+    this.num1 = Math.floor(Math.random() * 10);
+    this.num2 = Math.floor(Math.random() * 10);
+  }
+  checkAnswer(e: any) {
+    e.preventDefault();
+    let ans = this.num1 + this.num2;
+    if (ans == this.answer) {
+      this.randomGen();
+      console.log("hello");
+      this.totalAns += 1;
+      this.avg = this.totaltime / this.totalAns;
+      this.answer = '';
+      this.message = 'correct answer';
+    } else {
+      this.message = 'in-correct answer';
+      this.answer = '';
+    }
+  }
+  change(e: any) {
+    this.answer = Number(e.target.value);
+  }
 }
